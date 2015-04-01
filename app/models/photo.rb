bd42ca1db9bd0e46db9a0d8332a3a794dfcaa6b0
Lauren <lauren.taiclet@gmail.com>
2015-04-01 19:32:58 -0400

    Update forms to use simple_form

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,8 @@ class Photo < ActiveRecord::Base
 	mount_uploader :filename, PhotoUploader
 
 	belongs_to :user
+
+	validates :filename, presence: true
+	validates :caption, length: { minimum: 4 }
+	
 end
